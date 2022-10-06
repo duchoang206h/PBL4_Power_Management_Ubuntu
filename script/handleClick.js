ea142e2eeb2d57bc840623762288d14795c283f8
Đức Hoàng <hoanghd@bizverse.world>
2022-10-06 21:44:22 +0700

    feat: declare handleClick function

---
+++ script/handleClick.js
@@ -0,0 +1,93 @@
+const { MappingIndexToValue } = require('../config');
+const selects = document.querySelectorAll('select');
+selects.forEach(select => {
+    select.addEventListener('change', handleClick)
+})
+function handleClick (event){
+    const { id, selectedIndex } = event.target;
+    try {
+        switch (id) {
+            case "batteryTurnOff":
+                handleBatteryTurnOff(MappingIndexToValue.batteryTurnOff[selectedIndex])
+                break;
+            case "pluggedInTurnOn":
+                handlePluggedInTurnOn(MappingIndexToValue.pluggedInTurnOn[selectedIndex])
+                break;    
+            case "batterySleep":
+                handleBatterySleep(MappingIndexToValue.batterySleep[selectedIndex])
+                break;    
+            case "pluggedInSleep":
+                handlePluggedInSleep(MappingIndexToValue.pluggedInSleep[selectedIndex])
+                break;    
+            case "powerMode":
+                handlePowerMode(MappingIndexToValue.powerMode[selectedIndex])
+                break;    
+            case "batterySaveOn":
+                handleBatterySaveOn(MappingIndexToValue.batterySaveOn[selectedIndex])
+                break;    
+            case "batteryUsage":
+                handleBatteryUsage(MappingIndexToValue.batteryUsage[selectedIndex])
+                break;    
+            default:
+                break;
+        }
+    } catch (error) {
+        
+    }
+}
+function handleBatteryTurnOff (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+function handlePluggedInTurnOn (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+function handleBatterySleep (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+function handlePluggedInSleep (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+function handlePowerMode (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+function handleBatterySaveOn (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+function handleBatteryTurnOff (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+function handleBatteryUsage (value) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
