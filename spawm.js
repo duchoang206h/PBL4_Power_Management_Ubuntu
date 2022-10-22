031d2f4a5cc87135f0aa6c87ee7577cbc3792ee1
Đức Hoàng <hoanghd@bizverse.world>
2022-10-22 20:53:19 +0700

    feat: expose ipcMain, define handler

--- spawm.js
+++ spawm.js
@@ -1,5 +1,5 @@
 const { spawn, exec } = require("child_process");
-
+const { shell } = require('electron')
 exec('./shell.sh', (error, stdout, stderr) =>{
-    console.log(error, stdout)
- })
+    console.log(error, stdout, stderr)
+})
