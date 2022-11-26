0084340de603dc813262316384cbf2877f28dbb4
duchoang206h <duchoang206h@gmail.com>
2022-11-26 20:53:38 +0700

    fix: fix read setting file

--- handlers/setting.js
+++ handlers/setting.js
@@ -1,41 +1,40 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
+const settings = require("./setting.json");
 class SettingService {
-    constructor(){
-        this.settings =  JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'setting.json'), 'utf-8'))
-        console.log(this.settings);
+  constructor() {
+    this.settings = settings;
+  }
+  initSetting() {
+    try {
+      for (const [key, value] of Object.entries(this.settings)) {
+      }
+    } catch (error) {}
+  }
+  getSetting(field) {
+    return this.settings[field];
+  }
+  /**
+   *
+   * @returns { Promise<Boolean> }
+   */
+  updateSetting = async (field, value) => {
+    try {
+      if (this.settings.hasOwnProperty(field)) {
+        this.settings[field] = value;
+      }
+      // after handle update setting
+      await fs.promises.writeFile(
+        path.resolve(__dirname, "setting.json"),
+        JSON.stringify(this.settings)
+      );
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-    initSetting (){
-        try {
-            for(const [key, value] of Object.entries(this.settings)){
-
-            }
-        } catch (error) {
-            
-        }
-    }
-    getSetting (field) {
-        return this.settings[field]
-    }
-    /**
-     * 
-     * @returns { Promise<Boolean> }
-     */
-    updateSetting  = async (field, value) => {
-        try {
-            if(this.settings.hasOwnProperty(field)){
-                this.settings[field] = value;
-                
-            }
-            // after handle update setting
-            await fs.promises.writeFile(path.resolve(__dirname, '..', 'setting.json'), JSON.stringify(this.settings));
-            return true;
-        } catch (error) {
-            console.log(error)
-            return false;
-        }
-    }
+  };
 }
 module.exports = {
-    settingService: new SettingService()
-}
+  settingService: new SettingService(),
+};
