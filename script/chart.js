354b8fcc028c750cf775eaff9be111918ca4ff9e
Chu <chu>
2022-12-02 14:56:13 +0700

    fix conflict

--- script/chart.js
+++ script/chart.js
@@ -2,3 +2,14 @@ async function initData(){
     const data = await window.system.getBatteryHistory();
     console.log(data);
 }
+new Chartist.Line(
+    "#chart",
+    {
+        labels: ['24h', '22h', '20h', '18h', '16h', '14h', '12h', '10h', '8h', '6h' ,'4h' ,'2h', 'now'],
+        series: [[0,50, 90, 100, 80, 50, 100, 50, 40, 60, 100 ,80, 60 ]],
+    },
+    {
+        low: 0,
+        showArea: true,
+    }
+);
