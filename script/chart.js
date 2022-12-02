59b497c886c924bb825df8fe50632e432bcea0b7
duchoang206h <duchoang206h@gmail.com>
2022-12-02 14:53:30 +0700

    feat: add chart

--- script/chart.js
+++ script/chart.js
@@ -1,4 +1,18 @@
 async function initData(){
     const data = await window.system.getBatteryHistory();
-    console.log(data);
+    const labels = data.map(({ timestamps}) => new Date(timestamps*1000).toISOString()).slice(data.length - 5);
+    const levels = data.map(({ level}) => level).slice(data.length -5)
+    console.log(levels)
+    new Chartist.Line(
+        "#chart",
+        {
+          labels: labels,
+          series: [levels],
+        },
+        {
+          low: 0,
+          showArea: true,
+        }
+      );
 }
+initData()
